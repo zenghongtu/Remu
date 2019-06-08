73845e39e9dd9e4545245a24382519188a7ee324
zenghongtu <zenghongtu@qq.com>
2019-06-08 20:59:46 +0800

    feat(view-tab): add react-window for redner repos

--- src/view-tab/components/ReposBar/index.tsx
+++ src/view-tab/components/ReposBar/index.tsx
@@ -4,6 +4,7 @@ import RepoCard from './RepoCard';
 import './index.less';
 import { IStarredRepo } from '../../service';
 import { useState, useEffect, ChangeEvent, useRef } from 'react';
+import { FixedSizeList as VList } from 'react-window';
 
 interface IReposBar<S> {
   repos: S[];
@@ -13,18 +14,33 @@ const ReposBar = ({ repos, onSelect }: IReposBar<IStarredRepo>) => {
   const [filteredRepos, setFilteredRepos] = useState<IStarredRepo[]>(repos);
   const [searchFocus, setSearchFocus] = useState<boolean>(false);
   const [curRepoId, setCurRepoId] = useState<number>(null);
+  const [vListHeight, setVListHeight] = useState<number>(0);
   const searchInputRef = useRef(null);
+  const listWrapRef = useRef(null);
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeydown);
+    window.addEventListener('resize', handleResize);
+    updateVListHeight();
+
     return () => {
       document.removeEventListener('keydown', handleKeydown);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   useEffect(() => {
     setFilteredRepos(repos);
   }, [repos]);
+
+  const handleResize = () => {
+    updateVListHeight();
+  };
+
+  const updateVListHeight = () => {
+    const height = listWrapRef.current.offsetHeight;
+    setVListHeight(height);
+  };
 
   const handleKeydown = (e: KeyboardEvent) => {
     if (e.keyCode === 191) {
@@ -73,19 +89,34 @@ const ReposBar = ({ repos, onSelect }: IReposBar<IStarredRepo>) => {
         />
         {!searchFocus && <div className="search-slash">/</div>}
       </div>
-      <div className="reposbar-list">
+      <div className="reposbar-list-wrap" ref={listWrapRef}>
         {filteredRepos ? (
-          <List
-            dataSource={filteredRepos}
-            renderItem={(repo: IStarredRepo, index) => (
-              <List.Item onClick={handleReposItemClick(index)}>
-                <RepoCard
-                  repo={repo}
-                  isCurrentRepo={repo.repo.id === curRepoId}
-                />
-              </List.Item>
-            )}
-          />
+          <List>
+            <VList
+              height={vListHeight}
+              itemCount={filteredRepos.length}
+              itemSize={150}
+              width={400}
+              overscanCount={2}
+            >
+              {({ index, style }) => {
+                const repo = filteredRepos[index];
+                const {
+                  repo: { id },
+                } = repo;
+
+                return (
+                  <List.Item
+                    key={id}
+                    style={style}
+                    onClick={handleReposItemClick(index)}
+                  >
+                    <RepoCard repo={repo} isCurrentRepo={id === curRepoId} />
+                  </List.Item>
+                );
+              }}
+            </VList>
+          </List>
         ) : (
           <div className="reposbar-empty">
             <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
