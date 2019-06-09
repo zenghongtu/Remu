eb3443a4d6421c9bbea9caab23578c1165410643
zenghongtu <zenghongtu@qq.com>
2019-06-09 15:53:02 +0800

    chore: webpack optimize antd icons size

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -156,6 +156,9 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "@ant-design/icons/lib/dist$": path.resolve(__dirname, "../src/icons.ts"),
+    },
   },
   plugins: [
     ...plugins,
