8cdb7f1d5a7a871de37d2ec04f99cf25332863df
zenghongtu <zenghongtu@qq.com>
2019-06-09 16:24:36 +0800

    chore: webpack add build zip

--- webpack/webpack.analyzer.js
+++ webpack/webpack.analyzer.js
@@ -1,9 +1,11 @@
 const merge = require("webpack-merge");
-const prodConfig = require("./webpack.prod.js");
+const common = require("./webpack.common.js");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
 
-module.exports = merge(prodConfig, {
-  plugins: [new BundleAnalyzerPlugin()],
+module.exports = merge(common, {
+  mode: "production",
+  devtool: false,
+  plugins: [new CleanWebpackPlugin(), new BundleAnalyzerPlugin()],
 });
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,19 @@
+const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+const pkg = require("../package.json");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: false,
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ZipPlugin({
+      path: path.join(__dirname, "../"),
+      filename: `${pkg.name}-build-${pkg.version}`,
+      extension: "zip",
+    }),
+  ],
 });
