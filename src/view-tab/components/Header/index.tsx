d39caf3f9601b459d395c6c24cd01203fc3d9a98
zenghongtu <zenghongtu@qq.com>
2019-06-14 19:08:58 +0800

    feat: Header profile use hover

--- src/view-tab/components/Header/index.tsx
+++ src/view-tab/components/Header/index.tsx
@@ -213,7 +213,7 @@ const Header = ({ token }: IHeader) => {
                 </Menu.Item>
               </Menu>
             }
-            trigger={['click']}
+            trigger={['hover']}
           >
             <span>
               <img
